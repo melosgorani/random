4fe9cdc4c7d2cb780888719abe5a763de3ef9b51
Melos Gorani <melosgorani@Meloss-Air.home>
2024-02-13 19:05:02 -0500

    random

--- variables.js
+++ variables.js
@@ -1,13 +1,24 @@
 let name = 'Melos Gorani';
+
+console.log(typeof 'name' );
+
 let age = 26;
+
+console.log(typeof age);
+
 let isStudent = true 
+
+console.log(typeof isStudent);
+
 let favoriteColors = ['Blue','Black','Red'];
+
+console.log(typeof favoriteColors);
 
 let person = {
     firstName : 'Melos',
     lastName : 'Gorani',
-    age : 26
+    age : 26,
+    gender : 'M'
 };
-
-
+console.log(typeof person);
 console.log (person);
