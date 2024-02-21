a173be6de17514cdf6bf230f6acb263f14d29745
Melos Gorani <melosgorani@Meloss-Air.home>
2024-02-20 21:34:28 -0500

    challenge

---
+++ operatorsChallenge.js
@@ -0,0 +1,41 @@
+let num1 = 5;
+let num2 = 5;
+let total = num1 + num2;
+console.log(total);  
+
+let radius = 10;
+console.log(Math.PI * radius);
+
+let num = 5;
+num += 2;
+console.log(num);
+
+let price = 10;
+price *= 2.5;
+console.log(price);
+
+let age1 = 30;
+let age2 = 20;
+console.log(age1 > age2);
+
+let name1 = 'Melos';
+let name2 = 'John Doe';
+console.log(name1 != name2);
+
+let isMember = true;
+let hasCoupon = false;
+if(isMember || hasCoupon === true){
+    console.log('You are eligible for free shpping')
+};
+
+let num4 = 5;
+num = !0
+console.log(num);
+
+let age = 18;
+let isOldEnoughToVote = age >= 18 ? 'You are old enough to vote' : 'You are not old enough to vote';
+console.log(isOldEnoughToVote);
+
+let priceOfProduct = 100;
+let isProductOnSale = priceOfProduct = 100 ? 'The product is on sale' : 'The product is not on sale';
+console.log(isProductOnSale);
